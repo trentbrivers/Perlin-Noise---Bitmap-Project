6129257488230adb90958f179e1b2bca1cc6ecc1
Trent Rivers <trentbrivers@gmail.com>
2024-08-10 00:58:43 +0000

    small change

--- main_file.cpp
+++ main_file.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+double lerp(double t, double a, double b) {
+    //linear interpolation function
+    return a + t * (b - a); 
+}
+
 int main(){
     cout <<"This is a test. Project work to follow." << endl;
     return 0;
